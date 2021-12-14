53832d3e8bba8db9783f9cfa945b661758b52953
haimgoldfisher <haimngoldfisher@gmail.com>
2021-12-15 00:03:24 +0200

    try scanf

--- my_funcs.c
+++ my_funcs.c
@@ -1,22 +1,46 @@
 # include <stdio.h>
-# include <stdbool.h>
+# include <string.h>
 # include <stdlib.h>
+# include <ctype.h>
 # define TXT 1024
 # define WORD 30
 
-int gematria(char word[WORD], char text[TXT])
+int calc_gim(char *word)
 {
-    
+    int i;
+    int gim_val = 0;
+    for (i = 0; i < WORD; i++)
+    {
+        if (strcmp(word, "/0") == 0) // end of the word
+        {
+            break;
+        }
+        if (islower(*word))
+        {
+            gim_val += *word-96;
+        }
+        else if (isupper(*word))
+        {
+            gim_val += tolower(*word)-96;
+        }
+        word++;
+    }
+    return gim_val;
+}
+
+int gematria(char *word, char *text)
+{
+    int word_val = calc_gim(word);
     return 1;
 }
 
-int atbash(char word[WORD], char text[TXT])
+int atbash(char *word, char *text)
 {
 
     return 1;
 }
 
-int anagram(char word[WORD], char text[TXT])
+int anagram(char *word, char *text)
 {
 
     return 1;
--- stringProg.c
+++ stringProg.c
@@ -9,18 +9,19 @@
 int main()
 {
     char input[WORD+TXT];
-    scanf("%s", input); // scan the whole input into this string
+    scanf("%10[0-9a-zA-Z ]", &input); // scan the whole input into this string
     char *input_ptr = input; // pointer to the first char of the string
     char input_word[WORD]; // the word
     char input_text[TXT]; // the text
     int i;
     for (i = 0; i < WORD; i++)
     {
-        if (strcmp(input_ptr, "/0") == 0) // end of the first word
+        if (strcmp(input_ptr, "/0")) // end of the first word
         {
             break; // finish to fill the word
         }
-        input_word[i] = ++*input_ptr; // add the curr char to word
+        input_word[i] = *input_ptr; // add the curr char to word
+        input_ptr++;
     }
     i = 0;
     for (i = 0; i < TXT; i++)
@@ -29,14 +30,21 @@ int main()
         {
             break; // finish to fill the text
         }
-        input_text[i] = ++*input_ptr; // add the curr char to text
+        input_text[i] = *input_ptr; // add the curr char to text
+        input_ptr++;
     }
-    printf("%s", input_text);
-    printf("%s", input_word);
     
-    // printf("Gematria Sequences: " + gematria(input_word, input_text));
-    // printf("Atbash Sequences: " + atbash(input_word, input_text));
-    // printf("Anagram Sequences: " + anagram(input_word, input_text));
+    puts(input_word);
+    puts(input_text);
+    
+    // char *word_ptr = input_word;
+    // char *txt_ptr = input_text;
+    // printf("Gematria Sequences: ");
+    // gematria(word_ptr, txt_ptr);
+    // printf("Atbash Sequences: ");
+    // atbash(word_ptr, txt_ptr);
+    // printf("Anagram Sequences: ");
+    // anagram(word_ptr, txt_ptr);
 
     return 1;
 }
