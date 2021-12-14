e4338530b6646bd6c06a75e1512a18b9e81e0674
haimgoldfisher <haimngoldfisher@gmail.com>
2021-12-14 15:34:18 +0200

    main

--- stringProg.c
+++ stringProg.c
@@ -9,23 +9,24 @@ int main()
 {
     char input_word[WORD];
     char input_text[TXT];
-    int i, j;
+    int i;
     for (i = 0; i < WORD; i++)
     {
-        scanf("%d", &input_word[i]);
         if (input_word[i] ==' ' || input_word[i] =='\t'|| input_word[i] =='\n')
         {
             break;
         }
+        scanf("%d", &input_word[i]);
     }
-    for (j = 0; j < TXT; j++)
+    while (++i < TXT)
     {
-        scanf("%d",&input_text[j]);
-        if (input_text[j] == '~')
+        if (input_text[i] == '~')
         {
             break;
         }
+        scanf("%d",&input_text[i]);
     }
+    
     printf("Gematria Sequences: " + gematria(input_word, input_text));
     printf("Atbash Sequences: " + atbash(input_word, input_text));
     printf("Anagram Sequences: " + anagram(input_word, input_text));
